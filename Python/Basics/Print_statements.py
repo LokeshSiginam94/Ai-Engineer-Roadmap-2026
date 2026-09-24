a77799fc0d5cf9e007639c941bb3eb109b2aef4c
lokeshsiginam94 <lokeshsiginam199@gmail.com>
2026-09-24 17:39:49 +0530

    Add Basics

---
+++ Python/Basics/Print_statements.py
@@ -0,0 +1,5 @@
+print("Hello, World!")
+
+print(21)
+
+print(15+25)
---
+++ Python/Basics/variables.py
@@ -0,0 +1,25 @@
+name="Lokesh"
+age=21
+clg_name="SRKIT"
+percentile=93.5
+
+print(name)
+print(age)
+print(clg_name)
+print(percentile)
+
+#with sentance
+print("My name is",name)
+print("my age is",age)
+print("im studying in",clg_name)
+print("my percentile in mains was",percentile)
+
+#we can print everything in one line
+print("my name is",name,"im",age,"im studying in",clg_name)
+
+#type of variables
+print(type(age))
+print(type(name))
+print(type(percentile))
+print(type(clg_name))
+
