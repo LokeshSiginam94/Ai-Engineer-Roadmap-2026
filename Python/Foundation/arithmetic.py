448b9ca3baea2eab506b176e208ca6104bd7b36c
lokeshsiginam94 <lokeshsiginam199@gmail.com>
2026-09-25 10:33:21 +0530

    Add Foundation Q5

---
+++ Python/Foundation/arithmetic.py
@@ -0,0 +1,18 @@
+num1=int(input("Enter the first number:")) #int,float is accesable
+num2=int(input("Enter the secont number:")) #we can tahe 3 or we can also take 3.14
+
+#sum
+print("The sum of two numbers",num1+num2)
+#diff
+difference =num1-num2
+print("The diff of two numbers",difference)
+#mul
+print("The mul of two numbers",num1*num2)
+#div
+print("The div of two numbers",num1/num2)
+#modulo
+print("The mudl of two numbers",num1%num2)
+#pow
+print("The pow of two numbers",num1**num2)
+
+
