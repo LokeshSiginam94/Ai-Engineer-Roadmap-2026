ff34e1f442e8f61358b9618555f7197ceee7f533
lokeshsiginam94 <lokeshsiginam199@gmail.com>
2026-09-25 10:11:43 +0530

    Add Foundation Q2

---
+++ Python/Foundation/greeting.py
@@ -0,0 +1,9 @@
+name=str(input("Enter you name:"))
+age=int(input("Enter you age:"))
+
+# one sentance
+print("Hi",name,"You are",age,"years old")
+
+#two sentance
+print("Hi",name)
+print("Your age is:",age)
