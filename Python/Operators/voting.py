f4d9871fb0ef36ecc05b8639d0417a1fac1c5e13
lokeshsiginam94 <lokeshsiginam199@gmail.com>
2026-09-26 12:26:52 +0530

    Day-2 Q5

---
+++ Python/Operators/voting.py
@@ -0,0 +1,11 @@
+age=int(input("Enter your age:"))
+
+if age >=18:
+    print("You can vote!")
+else:
+    print("Not eligible")
+
+if age>=60:
+    print("Senior Discount eligible")
+else:
+    print("")   
