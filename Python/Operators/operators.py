566a0dbe43e2cc95b32580347835cf968d812c49
lokeshsiginam94 <lokeshsiginam199@gmail.com>
2026-09-26 10:31:03 +0530

    Day2 - Q1

---
+++ Python/Operators/operators.py
@@ -0,0 +1,6 @@
+a=int(input("Enter the value of a:"))
+b=int(input("Enter the value of b:"))
+
+print("The floor vlaue of a and b is:",a//b)
+print("The modulo value of a and b is:",a%b)
+print("The power valur of a up on b is:",a**b)
